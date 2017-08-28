fe9e382f425f70748a0762756c98277149190c92
konrad.jabrzyk@angrynerds.pl <konrad.jabrzyk@angrynerds.pl>
2017-08-28 14:15:09 +0200

    Details carousel created

---
+++ app/scripts/carousel-details.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items: 3,
+        lazyLoad: true,
+        loop: true,
+        dots: false,
+        margin: 10,
+        nav: true,
+    });
+});
