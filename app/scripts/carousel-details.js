0a2a36cdc91120cfac654404062342bb28bbf9dc
konrad.jabrzyk@angrynerds.pl <konrad.jabrzyk@angrynerds.pl>
2017-08-29 13:20:35 +0200

    Photos mobile carousel

--- app/scripts/carousel-details.js
+++ app/scripts/carousel-details.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
+    $('.carousel-details__owl').owlCarousel({
         items: 3,
         lazyLoad: true,
-        loop: true,
+        loop: false,
         dots: false,
         margin: 10,
         nav: true,
-        navText: ["<i class='fa fa-caret-left' aria-hidden='true'></i>","<i class='fa fa-caret-right' aria-hidden='true'></i>"]
+        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"],
+        responsive : {
+            0 : {
+                items: 2
+            },
+            768 : {
+                items: 4
+            }
+        },
+    });
+
+    $('.carousel-photos__owl').owlCarousel({
+        items: 1,
+        lazyLoad: true,
+        loop: true,
+        dots: false,
+        margin: 0,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"],
     });
 });
