f0f597a25251e7a8af735552ad29913c31724cbe
konrad.jabrzyk@angrynerds.pl <konrad.jabrzyk@angrynerds.pl>
2017-08-30 12:46:21 +0200

    Success message on valid contact form

--- app/scripts/form-validation.js
+++ app/scripts/form-validation.js
@@ -6,8 +6,18 @@
         if (form.checkValidity() == false) {
           event.preventDefault();
           event.stopPropagation();
+          form.classList.add("was-validated");
         }
-        form.classList.add("was-validated");
+        else {
+            event.preventDefault();
+            $.ajax({
+                url: location.href,
+                context: form
+            });
+            form.classList.remove("was-validated");
+            $('.modal-form__contact-form').hide();
+            $('.modal-form__submit-success').show();
+        }       
       }, false);
     }, false);
 }());
